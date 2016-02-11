05b11296d5cd947adaf5444950325148c6377284
Miroslav Vitkov <sir.vorac@gmail.com>
2016-02-12 00:40:53 +0200

    Now walking a folder with folders with images.

--- src/train.cpp
+++ src/train.cpp
@@ -8,6 +8,9 @@
 #include <vector>
 
 
+using namespace std;
+
+
 struct Images
 {
     // Input data to the recogniser training algorithm.
@@ -17,18 +20,27 @@ struct Images
 
     // Mapping from numeric labels to string names of persons.
     using Labels2Strings = std::map<int, std::string>;
-    Labels2Strings m;
+    using MapElem = Labels2Strings::value_type;
+    Labels2Strings maps;
 };
+
+
+// Returns true if entry is the unix current of parent folder.
+bool skip_auto_dirs(struct dirent *ent)
+{
+    bool is_currdir = !std::string(ent->d_name).compare( std::string(".") );
+    bool is_pardir  = !std::string(ent->d_name).compare( std::string("..") );
+    if(is_currdir || is_pardir)  return true;
+    else                         return false;
+}
 
 
 // Images are expected to be distributed among sub-folders inside a single folder.
 void read_images(const std::string &path, Images &out)
 {
     // Go to selected path.
-    DIR *dir;
-    struct dirent *ent;
-    dir = opendir(path.c_str());
-    if( dir == NULL)
+    DIR *parent_dir = opendir(path.c_str());
+    if( parent_dir == NULL)
     {
         std::cerr << "Could not open target location.";
         std::cerr << "Make sure path to a directory was supplied as argv[1]." << std::endl;
@@ -36,14 +48,33 @@ void read_images(const std::string &path, Images &out)
     }
 
     // Iterate each subfolder.
-  /* print all the files and directories within directory */
-    while ((ent = readdir (dir)) != NULL)
+    struct dirent *ent, *ent2;
+    while ( (ent = readdir(parent_dir)) != NULL )
     {
-        printf ("%s\n", ent->d_name);
+        if( skip_auto_dirs(ent) )  continue;
+
+        DIR *dir = opendir((std::string(path.c_str()) + std::string(ent->d_name)).c_str());
+        if(dir == NULL)
+        {
+            std::cerr << "Failed opening subdirectory " << ent->d_name << std::endl;
+            continue;
+        }
+
+        // Record the name of the folder, map a numeric counter to it.
+        unsigned counter = 0;
+        out.labels.push_back(counter);
+        out.maps.insert( Images::MapElem{counter, ent->d_name} );
+        ++counter;
+
+        // Read all images inside the folder.
+        while( (ent2 = readdir(dir)) != NULL )
+        {
+            if( skip_auto_dirs(ent2) )  continue;
+            printf("%s\n", ent2->d_name);
+        }
+        closedir(dir);
     }
-    closedir (dir);
-    // Record the name of the folder, map a numeric counter to it.
-    // Read all images inside the folder.
+    closedir(parent_dir);
 }
 
 
