05c802593c6312e272f2b9c6f248354bf24e76d7
Miroslav Vitkov <sir.vorac@gmail.com>
2016-12-14 19:15:44 +0200

    Refactoring on .csv file creation. Also tested to generate identical output as the previous python script.

--- src/train.cpp
+++ src/train.cpp
@@ -8,38 +8,36 @@
 #include <vector>
 
 
-using namespace std;
-
-
 struct Images
 {
+    using Id = int;
+
     // Input data to the recogniser training algorithm.
     // Needs to be exactly in this format.
-    std::vector<cv::Mat> images;
-    std::vector<int> labels;
+    std::vector<cv::Mat> _images;
+    std::vector<Id> _labels;
 
-    // Mapping from numeric labels to string names of persons.
-    using Labels2Strings = std::map<int, std::string>;
-    using MapElem = Labels2Strings::value_type;
-    Labels2Strings maps;
+    // Mapping from numeric ids to string names of persons.
+    std::map<Id, std::string> _id_to_name;
 };
 
 
-// Returns true if entry is the unix current of parent folder.
-bool skip_auto_dirs(struct dirent *ent)
+// Returns true if entry is the unix current of parent directory(dir).
+bool is_auto_dir( struct dirent *ent )
 {
-    bool is_currdir = !std::string(ent->d_name).compare( std::string(".") );
-    bool is_pardir  = !std::string(ent->d_name).compare( std::string("..") );
-    if(is_currdir || is_pardir)  return true;
-    else                         return false;
+    bool is_currdir = ! std::string( ent->d_name ).compare( "." );
+    bool is_pardir  = ! std::string( ent->d_name ).compare( ".." );
+    if( is_currdir || is_pardir )  return true;
+    else                           return false;
 }
 
 
-// Images are expected to be distributed among sub-folders inside a single folder.
-void read_images(const std::string &path, Images &out)
+// Images are expected to be distributed among sub-dirs inside a single dir.
+// One subdir per person.
+Images read_images( std::string path )
 {
     // Go to selected path.
-    DIR *parent_dir = opendir(path.c_str());
+    DIR *parent_dir = opendir( path.c_str() );
     if( parent_dir == NULL)
     {
         std::cerr << "Could not open target location.";
@@ -47,45 +45,57 @@ void read_images(const std::string &path, Images &out)
         std::exit(-1);
     }
 
-    // Iterate each subfolder.
-    struct dirent *ent, *ent2;
-    while ( (ent = readdir(parent_dir)) != NULL )
+    // Iterate each subdir.
+    unsigned i = 0;
+    Images out;
+    struct dirent *ent = NULL, *ent2 = NULL;
+    while( (ent = readdir(parent_dir)) != NULL )
     {
-        if( skip_auto_dirs(ent) )  continue;
+        if( is_auto_dir(ent) )  continue;
 
-        DIR *dir = opendir((std::string(path.c_str()) + std::string(ent->d_name)).c_str());
+        DIR *dir = opendir( (path + ent->d_name).c_str() );
         if(dir == NULL)
         {
             std::cerr << "Failed opening subdirectory " << ent->d_name << std::endl;
             continue;
         }
 
-        // Record the name of the folder, map a numeric counter to it.
-        unsigned counter = 0;
-        out.labels.push_back(counter);
-        out.maps.insert( Images::MapElem{counter, ent->d_name} );
-        ++counter;
-
-        // Read all images inside the folder.
+        // Read all images inside the dir.
         while( (ent2 = readdir(dir)) != NULL )
         {
-            if( skip_auto_dirs(ent2) )  continue;
-            printf("%s\n", ent2->d_name);
+            if( is_auto_dir(ent2) )  continue;
+            std::cout << path << ent->d_name << '/' << ent2->d_name  // relative path
+                      << ";"                                         // image-path;id separator
+                      << i << std::endl;
         }
-        closedir(dir);
+        closedir( dir );
+
+        // Record the name of the dir, map a numeric counter to it.
+        out._labels.push_back( i );
+        out._id_to_name.emplace( i, ent->d_name );
+        ++i;
+
     }
-    closedir(parent_dir);
+    closedir( parent_dir );
+
+    return out;
 }
 
 
-int main(int argc, char **argv)
+int main( int argc, char **argv )
 {
     // Get command-line parameters.
-    assert(argc == 2);
-    std::string path(argv[1]);
+    if( argc != 2 )
+    {
+        std::cout << "Usage: " << argv[0]
+                  << " path-to-dir-with-subdirs-with-images-for-each-person"
+                  << std::endl;
+        std::exit( -1 );
+    }
+    const std::string path( argv[1] );
 
-    Images images;
-    read_images(path, images);
+    // Output to stdout a .csv file, usable by  opencv's training algorithms.
+    read_images( path );
 
     return 0;
 }
