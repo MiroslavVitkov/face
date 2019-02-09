ae6407c592914ea15846f32a0cc537f27986aaa1
Miroslav Vitkov <sir.vorac@gmail.com>
2019-02-09 23:25:31 +0100

    [detect] Added code for recording videos.

--- src/detect.cpp
+++ src/detect.cpp
@@ -93,6 +93,48 @@ struct LBP : public FaceDetector
     }
 
 
+
+    void record_show_video()
+    {
+        cv::CascadeClassifier eyes_cascade( _settings.cascades_dir + "/haarcascade_eye_tree_eyeglasses.xml");
+        assert( ! eyes_cascade.empty() );
+
+            cv::Mat test;
+            _video_stream >> test;
+//        cv::VideoWriter vw( "kur.avi", cv:: VideoWriter::fourcc('P','I','M','1'), 20, test.size() );
+        cv::VideoWriter vw( "kur.avi", cv:: VideoWriter::fourcc('M','J','P','G'), 10, test.size() );
+
+        while(true)
+        {
+            cv::Mat frame;
+            _video_stream >> frame;
+              vw << frame;
+          detect( frame );
+            for( const auto & f : _facerect_buff )
+            {
+                cv::Point center( f.x + f.width/2, f.y + f.height/2 );
+                cv::ellipse( frame, center, cv::Size( f.width/2, f.height/2 ), 0, 0, 360, cv::Scalar( 255, 0, 255 ), 4 );
+
+                //-- In each face, detect eyes
+                std::vector<cv::Rect> eyes;
+                cv::Mat face{ frame, f };
+                assert( ! face.empty() );
+                eyes_cascade.detectMultiScale( face, eyes );
+                for ( const auto & e : eyes )
+                {
+                    cv::Point eye_center( f.x + e.x + e.width/2, f.y + e.y + e.height/2 );
+                    int radius = cvRound( (e.width + e.height)*0.25 );
+                    cv::circle( frame, eye_center, radius, cv::Scalar( 255, 0, 0 ), 4 );
+                }
+            }
+            _facerect_buff.clear();
+
+            cv::imshow( "Capture - Face detection", frame );
+            cv::waitKey(25);
+        }
+    }
+
+
 private:
     static cv::VideoCapture create_stream( const Settings & s )
     {
@@ -136,6 +178,7 @@ private:
 
     void detect( const cv::Mat & in )
     {
+        assert( ! in.empty() );
         assert( _facerect_buff.empty() );
 
         cv::Mat gray;
@@ -157,6 +200,5 @@ private:
 int main( int argc, char *argv[] )
 {
     LBP alg{ settings_run };
-    const auto f = alg.get_one_face();
-    cv::imwrite( "kur.jpg", f );
+    alg.record_show_video();
 }
