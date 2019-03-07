6ff3ee9d6b04812d12dba107734a6b9edd3e6994
Miroslav Vitkov <sir.vorac@gmail.com>
2019-03-07 22:03:53 +0100

    [io] Test cases added. Video recording still seems to exhibit too fash fps.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,39 @@
 
 #include <iostream>
 
+
+void cam_to_vid( const std::string fname_out = "cam.avi" )
+{
+    Camera c;
+    VideoWriter vw{ fname_out, c.get_size() };
+    cv::Mat f;
+    for( unsigned i = 0; i < 100; ++i )
+    {
+        c >> f;
+        vw << f;
+    }
+
+    std::cout << "Wrote video file " << fname_out << " from default camera/n";
+}
+
+
+void vid_to_vid( const std::string & fname_in = "cam.avi"
+               , const std::string & fname_out = "vid.avi" )
+{
+    VideoReader vr( fname_in );
+    VideoWriter vw( fname_out, vr.get_size() );
+    cv::Mat f;
+
+    while( vr >> f )
+    {
+        vw << f;
+    }
+
+    std::cout << "Copied video " << fname_in
+              << " frame by frame to " << fname_out + '\n';
+}
+
+
 void test()
 {
 
@@ -23,5 +56,6 @@ void test()
 
 int main()
 {
-    test();
+    cam_to_vid();
+    vid_to_vid();
 }
